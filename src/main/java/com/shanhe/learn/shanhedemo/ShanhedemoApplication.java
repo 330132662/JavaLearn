888cf2f0495e3d649c2502900537d64962c49a4b
juhui <330132662@qq.com>
2023-09-01 10:56:28 +0800

    感谢我的好兄弟王种  耗费一上午时间  跑通demo

---
+++ src/main/java/com/shanhe/learn/shanhedemo/ShanhedemoApplication.java
@@ -0,0 +1,13 @@
+package com.shanhe.learn.shanhedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanhedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShanhedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shanhe/learn/shanhedemo/ShanhedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanhe.learn.shanhedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShanhedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
